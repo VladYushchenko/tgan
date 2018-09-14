5d03660b0a3e81f23aa1fd408a49f23830ea75e6
VladYushchenko <yushchenko.vladyslav@gmail.com>
2018-09-14 02:58:17 +0200

    Back to tgan version of IncScore

--- evaluation/calc_inception.py
+++ evaluation/calc_inception.py
@@ -27,27 +27,29 @@ def get_inception(c3dmodel, args):
     """Launch chainer and extract inception score"""
     xp = chainer.cuda.cupy
     mean = np.load(args.mean)
-    mean = mean.reshape((3, 1, args.video_length, args.image_size, args.image_size))
+    mean = mean.reshape((3, 1, 16, args.image_size, args.image_size))
+    mean = np.mean(mean, axis=2, keepdims=True)
     loader = Loader(args)()
 
     ys = []
     for _, (x, _) in tqdm(enumerate(loader), total=len(loader)):
         x = x.data.cpu().numpy()
-        x = x.transpose(1, 0, 2, 3, 4)
-        x = (x + 1) / 2 * 255
-        x = x.astype('uint8')
+        # x = x.transpose(1, 0, 2, 3, 4)
+        # x = (x + 1) / 2 * 255
+        # # Needed to equal images loaded from disk vs directly generated images
+        # # While saving from float32 to uint8 [0 .. 255], using this normalization
+        # # x = np.around(x).astype('uint8')
+        # x = x.astype('uint8')
 
         # TODO: use for direct comparison with TGAN paper (use INTER_CUBIC)
-        # n, c, f, h, w = x.shape
-        # x = x.transpose(0, 2, 3, 4, 1).reshape(n * f, h, w, c)
-        # x = (x + 1) / 2 * 255
-        # x = x.astype('uint8')
-        # x_ = np.zeros((n * f, args.image_size, args.image_size, 3))
-        # for t in range(n * f):
-        #     x_[t] = np.asarray(
-        #         cv.resize(x[t], (args.image_size, args.image_size), interpolation=args.interpolation))
-        # x = x_.transpose(3, 0, 1, 2).reshape(3, n, f, args.image_size, args.image_size)
-        # x = x.astype('uint8')
+        n, c, f, h, w = x.shape
+        x = x.transpose(0, 2, 3, 4, 1).reshape(n * f, h, w, c)
+        x = x * 128 + 128
+        x_ = np.zeros((n * f, args.image_size, args.image_size, 3))
+        for t in range(n * f):
+            x_[t] = np.asarray(
+                cv.resize(x[t], (args.image_size, args.image_size), interpolation=args.interpolation))
+        x = x_.transpose(3, 0, 1, 2).reshape(3, n, f, args.image_size, args.image_size)
 
         # mean file is BGR-order while model outputs RGB-order
         x = x[::-1] - mean
@@ -62,28 +64,9 @@ def get_inception(c3dmodel, args):
     return ys
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Tgan inception score')
-    parser.add_argument('--model', type=str)
-    parser.add_argument('--dataset', type=str, default=None)
-    parser.add_argument('--result_dir', default='./result')
-    parser.add_argument('--cuda', action='store_false')
-    parser.add_argument('--device', type=int, default=0)
-    parser.add_argument('--iter', type=int, default=100000)
-    parser.add_argument('--calc_iter', type=int, default=10000)
-    parser.add_argument('--mean', type=str, default='/home/vlad/Downloads/crop_mean.npy')
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--batch_size', type=int, default=8)
-    parser.add_argument('--image_size', type=int, default=112)
-    parser.add_argument('--video_length', type=int, default=16)
-    parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--ext', default='jpg')
-    parser.add_argument('--interpolation', type=str, default='INTER_CUBIC')
-    args = parser.parse_args()
-
+def calculate_inception_score(args):
     np.random.seed(args.seed)
 
-    inter_method = args.interpolation
     args.interpolation = getattr(cv, args.interpolation)
 
     cuda.get_device(args.device).use()
@@ -96,12 +79,30 @@ def main():
 
     score = calc_inception(ys)
     print(score)
-    with open('{}/inception_iter-{}_{}.txt'.format(args.result_dir, args.iter, inter_method), 'w') as fp:
-        print(args.result_dir, args.iter, args.calc_iter, args.mean, score, file=fp)
-        print(args.result_dir, args.iter, 'score:{}'.format(score))
 
-    return 0
+    # with open('{}/inception_iter-{}_{}.txt'.format(args.result_dir, args.iter, inter_method), 'w') as fp:
+    #     print(args.result_dir, args.iter, args.calc_iter, args.mean, score, file=fp)
+    #     print(args.result_dir, args.iter, 'score:{}'.format(score))
+    return score
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    parser = argparse.ArgumentParser(description='Tgan inception score')
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--dataset', type=str, default=None)
+    parser.add_argument('--result_dir', default='./result')
+    parser.add_argument('--cuda', action='store_false')
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--iter', type=int, default=100000)
+    parser.add_argument('--calc_iter', type=int, default=10000)
+    parser.add_argument('--mean', type=str, default='/home/vlad/Downloads/crop_mean.npy')
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--image_size', type=int, default=112)
+    parser.add_argument('--video_length', type=int, default=16)
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--ext', default='jpg')
+    parser.add_argument('--interpolation', type=str, default='INTER_CUBIC')
+    args = parser.parse_args()
+
+    calculate_inception_score(args)
